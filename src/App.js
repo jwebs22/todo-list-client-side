c58bfb62e1b08c8c7aee6765b8f4c9ab22bdd624
dd-code-immersives <domilamad89@gmail.com>
2023-03-15 12:19:50 -0400

    installed react-router-dom made directories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useState, useEffect } from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+function App() {  
+
+  //set up hooks for the state 
+  const [toDoList, setToDoList] = useState([]);
+
+
+  //load the todo items from the back end 
+  useEffect(() => {
+
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
